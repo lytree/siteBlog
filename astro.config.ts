d250bfa71fdad8120897da8eeba241ad253ca4e6
lytree <hi.yangzh@outlook.com>
2024-01-28 22:26:29 +0800

    update

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,12 @@ import vue from '@astrojs/vue';
 import mdx from '@astrojs/mdx';
 import UnoCSS from 'unocss/astro'
 import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), vue({ appEntrypoint: "./src/_app" }), UnoCSS({ injectReset: true }),],
+	integrations: [
+		mdx(),
+		sitemap(),
+		vue({ appEntrypoint: "./src/_app", reactivityTransform: true }),
+		UnoCSS({ injectReset: true })]
 });
---
+++ src/composables/dark.ts
@@ -0,0 +1,10 @@
+import { useDark, useToggle } from '@vueuse/core'
+
+export const isDark = useDark({
+  selector: 'html',
+  storageKey: 'theme-appearance',
+  valueDark: 'dark',
+  valueLight: 'light',
+})
+
+export const toggleDark = useToggle(isDark); 
