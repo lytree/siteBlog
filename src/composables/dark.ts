2727f41f76f4d71316ea1b6d2ed60f6466d88092
lytree <hi.yangzh@outlook.com>
2024-02-03 22:06:57 +0800

    update layout

--- src/composables/dark.ts
+++ src/composables/dark.ts
@@ -2,9 +2,9 @@ import { useDark, useToggle } from '@vueuse/core'
 
 export const isDark = useDark({
   selector: 'html',
-  storageKey: 'theme-appearance',
+  storageKey: 'data-theme',
   valueDark: 'dark',
   valueLight: 'light',
 })
 
-export const toggleDark = useToggle(isDark); 
+export const toggleDark = useToggle(isDark);
