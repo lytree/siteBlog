0e8b07c3a68061063dfd3d7d4cfcd96dc3ced725
lytree <hi.yangzh@outlook.com>
2024-03-17 23:28:16 +0800

    update layout

--- src/components/docs/PostCard.vue
+++ src/components/docs/PostCard.vue
@@ -4,7 +4,7 @@ import Card from 'primevue/card';
 const { defaultCover, post } = defineProps(["defaultCover", "post"]);
 
 const goToUrl = (slug: string) => {
-    siteUtils.goToUrl(slug);
+    siteUtils.goToUrl("docs/post/" + slug);
 }
 
 </script>
@@ -39,22 +39,54 @@ const goToUrl = (slug: string) => {
             </div>
         </div>
     </div> -->
-    <Card class="mx-auto" style="width: 60%; overflow: hidden">
+    <Card class="mx-auto my-4 " style="width: 80%; overflow: hidden" @click=goToUrl(post.slug)>
         <template #header>
-            <img :src="`${post.data.cover || defaultCover}`" />
+            <img class="transition ease-in-out hover:-translate-2 hover:scale-110 duration-300"
+                :src="`${post.data.cover || defaultCover}`" />
+            <a class="post-card-category" :href="`/categories/${post.data.category}`">
+                {{ post.data.category }}
+            </a>
         </template>
         <template #title>{{ post.data.title }}</template>
-        <template #subtitle>Card subtitle</template>
         <template #content>
             <p class="m-0">
                 {{ post.data.description }}
             </p>
         </template>
         <template #footer>
-            <div class="flex gap-3 mt-1">
-                <Button label="Cancel" severity="secondary" outlined class="w-full" />
-                <Button label="Save" class="w-full" />
+            <ul class="post-card-tags">
+                <li v-for="tag in post.data.tags" class="post-card-tag">
+                    <a v-if="tag && tag != ''" :href="`/tags/${tag}`">{{ tag }}</a>
+                </li>
+
+            </ul>
+            <div class="post-card-update">
+                <a href="/">
+                    {{ siteUtils.formatDate(post.data.updatedDate || post.data.pubDate) }}
+                </a>
             </div>
         </template>
     </Card>
 </template>
+<style lang="scss">
+.p-card {
+    --at-apply: flex h-64;
+    cursor: pointer;
+
+    &-header {
+        --at-apply: h-full flex justify-center;
+        transition: .3s ease-in-out;
+        border-radius: 15px 0 0 15px;
+
+    }
+
+    &-body {
+        --at-apply: h-full flex flex-col col-6 ml-4;
+
+    }
+
+    &-footer {
+        --at-apply: justify-center;
+    }
+}
+</style>
