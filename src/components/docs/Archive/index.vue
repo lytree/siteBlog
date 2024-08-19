9703e4e374d8d018d8364ab6959e6cd9fc9c28be
yangzh <yangzh@windit.com.cn>
2024-08-19 17:55:05 +0800

    update Archive Layout

--- src/components/docs/Archive/index.vue
+++ src/components/docs/Archive/index.vue
@@ -1,6 +1,6 @@
 <script lang="tsx" setup>
 
-import Timeline from 'primevue/timeline';
+import DataView from 'primevue/dataview';
 import ClientOnly from '@components/common/ClientOnly';
 
 const { posts } = defineProps(["posts"]);
@@ -8,11 +8,31 @@ const { posts } = defineProps(["posts"]);
 console.log(posts);
 </script>
 <template>
-    <ClientOnly>
+    <!-- <ClientOnly>
         <Timeline pt:root="w-full" :value="posts">
             <template #content="slotProps">
                 {{ slotProps.item.data.title }}
             </template>
-        </Timeline>
-    </ClientOnly>
+</Timeline>
+</ClientOnly> -->
+    <DataView :value="posts" class="w-full " paginator :rows="8">
+        <template #list="slotProps">
+            <div class="flex flex-col">
+                <div v-for="(item, index) in slotProps.items" :key="index">
+                    <div class="flex flex-col sm:flex-row sm:items-center p-6 gap-4"
+                        :class="{ 'border-t border-surface-200 dark:border-surface-700': index !== 0 }">
+                        <div class="flex flex-col md:flex-row justify-between md:items-center flex-1 gap-6">
+                            <div class="flex flex-row md:flex-col justify-between items-start gap-2">
+
+                            </div>
+                            <div class="flex flex-col md:items-end gap-8">
+                                
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </DataView>
 </template>
