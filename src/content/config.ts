199609c4e66471e4fa92d1142ed0e714070785ac
lytree <hi.yangzh@outlook.com>
2024-09-27 20:44:45 +0800

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,5 +23,27 @@ const docs = defineCollection({
 		nextSlug: z.string().default(''),
 	}),
 });
+const exam = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		// Transform string to Date object
+		permalink: z.string().optional(),
+		date: z.coerce.date(),
+		lastmod: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		draft: z.boolean().optional().default(false),
+		description: z.string().optional().default(''),
+		image: z.string().optional().default(''),
+		tags: z.array(z.string()).optional().default([]),
+		category: z.string().optional().default("uncategorized"),
 
-export const collections = { docs };
+		/* For internal use */
+		prevTitle: z.string().default(''),
+		prevSlug: z.string().default(''),
+		nextTitle: z.string().default(''),
+		nextSlug: z.string().default(''),
+	}),
+});
+export const collections = { "docs": docs, "exam": exam };
