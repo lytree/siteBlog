ff06760e17d43f33a475c6ca863d60e4f5037bb9
yangzh <yangzh@windit.com.cn>
2024-10-28 16:15:43 +0800

    add starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,10 @@ import swup from '@swup/astro';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeComponents from "rehype-components"; /* Render the custom directive content */
+import rehypeComponents from "rehype-components";/* Render the custom directive content */
 import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
-import remarkDirective from "remark-directive"; /* Handle directives */
+import remarkDirective from "remark-directive";/* Handle directives */
 import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
 import remarkMath from "remark-math";
 import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.mjs";
@@ -19,103 +19,110 @@ import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import icon from "astro-icon";
 import yaml from '@rollup/plugin-yaml';
 
+import starlight from "@astrojs/starlight";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.prideyang.top",
-	devToolbar: {
-		enabled: false
-	},
-	trailingSlash: "always",
-	markdown: {
-		remarkPlugins: [remarkMath, remarkReadingTime, remarkExcerpt, remarkGithubAdmonitionsToDirectives, remarkDirective, parseDirectiveNode],
-		rehypePlugins: [rehypeKatex, rehypeSlug,
-			[rehypeComponents, {
-				components: {
-					github: GithubCardComponent,
-					note: (x, y) => AdmonitionComponent(x, y, "note"),
-					tip: (x, y) => AdmonitionComponent(x, y, "tip"),
-					important: (x, y) => AdmonitionComponent(x, y, "important"),
-					caution: (x, y) => AdmonitionComponent(x, y, "caution"),
-					warning: (x, y) => AdmonitionComponent(x, y, "warning")
-				}
-			}],
-			[rehypeAutolinkHeadings, {
-				behavior: "append",
-				properties: {
-					className: ["anchor"]
-				},
-				content: {
-					type: "element",
-					tagName: "span",
-					properties: {
-						className: ["anchor-icon"],
-						'data-pagefind-ignore': true
-					},
-					children: [{
-						type: "text",
-						value: "#"
-					}]
-				}
-			}]],
-		// drafts: true,
-		shikiConfig: {
-			theme: 'dracula',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				theme: 'dracula',
-				wrap: true
-			}
-			// drafts: true
-		}), sitemap(), swup({
-			theme: false,
-			animationClass: 'transition-swup-',
-			// see https://swup.js.org/options/#animationselector
-			// the default value `transition-` cause transition delay
-			// when the Tailwind class `transition-all` is used
-			containers: ['main'],
-			smoothScrolling: true,
-			cache: true,
-			preload: true,
-			accessibility: true,
-			updateHead: true,
-			updateBodyClass: false,
-			globalInstance: true
-		}), vue({
-			appEntrypoint: "./src/_app"
-		}), tailwind({}), icon({
-			include: {
-				"material-symbols": ["*"],
-				"fa6-brands": ["*"],
-				"fa6-regular": ["*"],
-				"fa6-solid": ["*"],
-				"mdi": ["*"],
-			},
-		}),
+    site: "https://www.prideyang.top",
+    devToolbar: {
+        enabled: false
+    },
+    trailingSlash: "always",
+    markdown: {
+        remarkPlugins: [remarkMath, remarkReadingTime, remarkExcerpt, remarkGithubAdmonitionsToDirectives, remarkDirective, parseDirectiveNode],
+        rehypePlugins: [rehypeKatex, rehypeSlug,
+            [rehypeComponents, {
+                components: {
+                    github: GithubCardComponent,
+                    note: (x, y) => AdmonitionComponent(x, y, "note"),
+                    tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+                    important: (x, y) => AdmonitionComponent(x, y, "important"),
+                    caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+                    warning: (x, y) => AdmonitionComponent(x, y, "warning")
+                }
+            }],
+            [rehypeAutolinkHeadings, {
+                behavior: "append",
+                properties: {
+                    className: ["anchor"]
+                },
+                content: {
+                    type: "element",
+                    tagName: "span",
+                    properties: {
+                        className: ["anchor-icon"],
+                        'data-pagefind-ignore': true
+                    },
+                    children: [{
+                        type: "text",
+                        value: "#"
+                    }]
+                }
+            }]],
+        // drafts: true,
+        shikiConfig: {
+            theme: 'dracula',
+            wrap: true
+        }
+    },
+    integrations: [
+        starlight({
+            title: '考试',
+        }),
+        mdx({
+            syntaxHighlight: 'shiki',
+            shikiConfig: {
+                theme: 'dracula',
+                wrap: true
+            }
+            // drafts: true
+        }),
+        sitemap(),
+        swup({
+            theme: false,
+            animationClass: 'transition-swup-',
+            // see https://swup.js.org/options/#animationselector
+            // the default value `transition-` cause transition delay
+            // when the Tailwind class `transition-all` is used
+            containers: ['main'],
+            smoothScrolling: true,
+            cache: true,
+            preload: true,
+            accessibility: true,
+            updateHead: true,
+            updateBodyClass: false,
+            globalInstance: true
+        }),
+        vue({
+            appEntrypoint: "./src/_app"
+        }),
+        tailwind({}),
+        icon({
+            include: {
+                "material-symbols": ["*"],
+                "fa6-brands": ["*"],
+                "fa6-regular": ["*"],
+                "fa6-solid": ["*"],
+                "mdi": ["*"],
+            },
+        })],
+    vite: {
+        plugins: [yaml()],
+        build: {
+            rollupOptions: {
+                output: {
+                    assetFileNames: `assets/[name].[ext]`,
+                },
+                onwarn(warning, warn) {
+                    // temporarily suppress this warning
+                    if (warning.message.includes("is dynamically imported by") && warning.message.includes("but also statically imported by")) {
+                        return;
+                    }
+                    warn(warning);
+                },
+                external: ["fsevents", new RegExp('/patches/.*')]
+            }
 
-	],
-	vite: {
-		plugins: [yaml()],
-
-		build: {
-			rollupOptions: {
-				output: {
-					assetFileNames: `assets/[name].[ext]`,
-				},
-				onwarn(warning, warn) {
-					// temporarily suppress this warning
-					if (warning.message.includes("is dynamically imported by") && warning.message.includes("but also statically imported by")) {
-						return;
-					}
-					warn(warning);
-				},
-				external: ["fsevents", new RegExp('/patches/.*')]
-			}
-
-		}
-	}
+        }
+    }
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from 'astro:content';
-
+import { docsSchema } from '@astrojs/starlight/schema';
 const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
@@ -24,27 +24,6 @@ const blog = defineCollection({
 	}),
 });
 
-const docs = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		// Transform string to Date object
-		permalink: z.string().optional(),
-		date: z.coerce.date(),
-		lastmod: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		draft: z.boolean().optional().default(false),
-		description: z.string().optional().default(''),
-		image: z.string().optional().default(''),
-		tags: z.array(z.string()).optional().default([]),
-		category: z.string().optional().default("uncategorized"),
 
-		/* For internal use */
-		prevTitle: z.string().default(''),
-		prevSlug: z.string().default(''),
-		nextTitle: z.string().default(''),
-		nextSlug: z.string().default(''),
-	}),
-});
-export const collections = { "blog": blog, "docs": docs };
+
+export const collections = { "blog": blog, "docs": { schema: docsSchema() } };
