33b6028a3a1e6ab4462974beefed4691998a45df
lytree <hi.yangzh@outlook.com>
2024-10-31 22:59:00 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -103,6 +103,13 @@ export default defineConfig({
         })],
     vite: {
         plugins: [yaml()],
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import']
+                },
+            }
+        },
         build: {
             rollupOptions: {
                 output: {
