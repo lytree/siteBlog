f43a62b3aae29069451c8cfe67c0843694287e47
lytree <hi.yangzh@outlook.com>
2024-11-03 09:41:25 +0800

    update layout

--- src/components/vue/ColorSwitch.vue
+++ src/components/vue/ColorSwitch.vue
@@ -4,7 +4,7 @@
             <button id="theme-toggle" data-tooltip-target="tooltip-toggle" type="button"
                 :class="isDark ? 'icon-[material-symbols--mode-night]' : 'icon-[material-symbols--sunny]'"
                 aria-label="Toggle dark mode" @click="toggleDark()"
-                class="text-gray-500 inline-flex items-center justify-center dark:text-gray-400 hover:bg-gray-100 w-5 h-5 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+                class="inline-flex items-center justify-center w-5 h-5  focus:outline-none rounded-lg p-2">
             </button>
         </ClientOnly>
     </div>
